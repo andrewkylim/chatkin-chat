7e251c9382a56e8027530a3b85ecab31bc6428ff
andrewkylim <andrewkylim@gmail.com>
2025-11-30 15:22:19 +0000

    Update FileUpload.svelte

--- apps/web/src/lib/components/FileUpload.svelte
+++ apps/web/src/lib/components/FileUpload.svelte
@@ -174,7 +174,7 @@
 		{accept}
 		on:change={handleFileSelect}
 		disabled={uploading}
-		style="display: none;"
+		style="position: absolute; width: 1px; height: 1px; padding: 0; margin: -1px; overflow: hidden; clip: rect(0, 0, 0, 0); white-space: nowrap; border: 0;"
 	/>
 
 	{#if showDragDrop}
